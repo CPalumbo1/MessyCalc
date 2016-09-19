623cb439554f2dfb650c45ec42000bac0df5e78f
Colton A Palumbo <cp352@pvhs-lab112-d11.pvsd.org>
2016-09-19 14:29:53 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner calc = new Scanner(System.in);
+       System.out.println("Enter two numbers that you want to add");
+        int i = calc.nextInt();
+        System.out.println( * i);
+
+
+
+    }
+}
